e951084261d7145296282e94a0e9fe8ca4f6c0da
andremoss800 <andremoss800@gmail.com>
2024-11-12 21:07:08 +0200

    recommit

--- lib/screens/business_screens/add_offer.dart
+++ lib/screens/business_screens/add_offer.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:offers_app/providers/usertype_provider.dart';
+import 'package:offers_app/providers/user_provider.dart';
 
 //PROSEXE NA BALEIS NA MI MPOROUN NA KANOUN ADD OFFER BUSINESSESS POU DEN EXOUN BALEI PEDIO ONOMA BUSINESS
 // I NA TO ELEGXEIS. GIATI DEN EXW BALEI GIA OLES AKOMA.
--- lib/widgets/offer_tile.dart
+++ lib/widgets/offer_tile.dart
@@ -14,26 +14,6 @@ class OfferTile extends StatefulWidget {
 }
 
 class _OfferTileState extends State<OfferTile> {
-  Future<String?> fetchProfileImage(String businessId) async {
-    try {
-      final docSnapshot = await FirebaseFirestore.instance
-          .collection('users')
-          .doc(businessId)
-          .get();
-
-      // `profile_image`Elegxos an to eggrafo iparxei kai an periexo to pedio profile_image
-      if (docSnapshot.exists && docSnapshot.data() != null) {
-        final data = docSnapshot.data()!;
-        return data.containsKey('profile_image')
-            ? data['profile_image'] as String
-            : null;
-      }
-    } catch (e) {
-      print('Error fetching profile image: $e');
-    }
-    return null;
-  }
-
   @override
   Widget build(BuildContext context) {
     return InkWell(
